ab599efddc365416c91bcd650b877fda6b6d34c2
Simone D'Amico <sim@me.com>
2015-09-15 20:32:30 +0100

    Shouldn't this be public?

--- GEOSwift/MapKit.swift
+++ GEOSwift/MapKit.swift
@@ -114,7 +114,7 @@ MKShape subclass for GeometryCollections.
 The property `shapes` contains MKShape subclasses instances. When drawing shapes on a map be careful to the fact that that these shapes could be overlays OR annotations.
 */
 public class MKShapesCollection : MKShape, MKAnnotation, MKOverlay  {
-    let shapes: Array<MKShape>
+    public let shapes: Array<MKShape>
     public let centroid: CLLocationCoordinate2D
     public let boundingMapRect: MKMapRect
     
